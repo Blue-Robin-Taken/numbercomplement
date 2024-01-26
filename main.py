bed6acadc6e265d9ce14af4072881803e0a0eb22
Blue-Robin-Taken <86581171+Blue-Robin-Taken@users.noreply.github.com>
2024-01-26 22:04:12 +0000

    broken 8s

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import re
 
-complement = 10
+complement = 8
 
 with open('input.txt', 'r') as f:
     r = f.read()
@@ -21,26 +21,40 @@ with open('input.txt', 'r') as f:
         for char in list(number):
             if complement == 9 or complement == 10:
                 add_num += ' '+str((9)-int(char))
+            if complement == 8 or complement == 7:
+                if 7 - int(char) >= 0:
+                    add_num += str((7)-int(char))
+                else:
+                    add_num += "0"
+        if complement == 10:
+            add_num = add_num[:-1] + str(1+int(add_num[-1]))
+        if complement == 8:
+            add_num = add_num[:-1] + str(1+int(add_num[-1]))
 
-        add_num = add_num[:-1] + str(1+int(add_num[-1]))
 
         return_list.append(add_num)
 
-    # --- Remove numbers greater than 10 ---
-    num_index = 0
-    for cool_thing in return_list:
-        #for i in range(len(cool_thing.strip().split(' '))):
-        thing_index = 0
-        cool_thing = cool_thing.strip()
-        for thing in cool_thing.strip().split(' '):
-            thing = thing.replace(' ', '')
-            if thing:
-                if int(thing.strip()) > 9:
-                    print(cool_thing)
-                    return_list[num_index] =' '+"".join(cool_thing.split(' ')[:thing_index-1])+ str(int(cool_thing.split(' ')[thing_index-1])+1)  + '0'
-                    print(return_list[num_index])
-            thing_index += 1
-        num_index += 1
-    for thing in return_list:
-        print("{:,}".format(int(thing.replace(' ', ''))))
+    if complement == 10 or complement == 8:
+        # --- Remove numbers greater than 10 ---
+        num_index = 0
+        for cool_thing in return_list:
+            #for i in range(len(cool_thing.strip().split(' '))):
+            thing_index = 0
+            cool_thing = cool_thing.strip()
+            for thing in cool_thing.strip().split(' '):
+                thing = thing.replace(' ', '')
+                if thing:
+                    if int(thing.strip()) > complement-1:
+                        return_list[num_index] =' '+"".join(cool_thing.split(' ')[:thing_index-1])+ str(int(cool_thing.split(' ')[thing_index-1])+1)  + '0'
+                thing_index += 1
+            num_index += 1
+            
+    if complement == 9 or complement == 10:
+        for thing in return_list:
+            print("{:,}".format(int(thing.replace(' ', ''))))
+    if complement == 8 or complement == 7:
+        for thing in return_list:
+            a= thing.strip()
+            print(' '.join([a[i:i+3] for i in range(0, len(a), 3)]))
+            # https://stackoverflow.com/questions/15254195/python-how-to-add-space-on-each-3-characters
     print(return_list)
