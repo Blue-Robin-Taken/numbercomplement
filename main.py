545635de3d7d0d203e7c7c76740261114a6a8ebf
Blue-Robin-Taken <86581171+Blue-Robin-Taken@users.noreply.github.com>
2024-01-26 21:27:07 +0000

    e

--- main.py
+++ main.py
@@ -1,63 +1,41 @@
-import re
-
-new_l = []  # list of questions
-with open('input.txt', 'r') as f:
-    r = f.read().split('.')  # read file
-    for i in r:  # make a list out of every question
-        new_l.append(re.sub('[^0-9]','', i).strip())
-
-# --- Remove blanks ---
-print(new_l)
-new_l_ = []
-for i in new_l:
-    if i:
-        new_l_.append(i)
-
-new_l = new_l_
-print(new_l)
-    
-return_str = ""  # the string to return at the end
-print(new_l)
-for question in new_l:
-    question_return = ""  # the return for each question
-    start_number = False
-    
-    # --- Base calculation ---
-    for character in question:
-        if not start_number:
-            if int(character) > 0:
-                question_return += str(7-int(character))
-            else:
-                question_return += "0"
-    print(question_return[-1])
-    
-    # --- Fixing values over 7 ---
-    ind = 0
-    temp_val_store = ""
-    add_one = False
-    for char in question_return:
-        if add_one:
-            temp_val_store += str(int(char)+1)
-        else:
-            temp_val_store += char
-
-        if int(char[-1]) > 7:
-            temp_val_store += "0"
-            add_one = True
-        else:
-            add_one = False
-        ind += 1
-        
-        
-    temp_q_store = ""  # used for storing the temporary formatting
-    ind=0  # index of formatting within the return string
-
-    # --- Formatting ---
-    for char_format in temp_val_store:
-        if ind%3 == 0:
-            temp_q_store += ' '
-        temp_q_store += char_format
-        ind += 1
-
-    return_str += temp_q_store.strip() + '\n'
-print("RETURNING \n\n", return_str)
+import re                                                                                                                                                                                                          
+                                                                                                                                                                                                                   
+with open('input.txt', 'r') as f:                                                                                                                                                                                  
+    r = f.read()                                                                                                                                                                                                   
+    new_list = []                                                                                                                                                                                                  
+    for line in r.split('.'):                                                                                                                                                                                      
+        new_list.append(re.sub("[^0-9]", "", line))                                                                                                                                                                
+                                                                                                                                                                                                                   
+    new_list_ = []                                                                                                                                                                                                 
+    for line in new_list:                                                                                                                                                                                          
+        if line:                                                                                                                                                                                                   
+            new_list_.append(line)                                                                                                                                                                                 
+    new_list = new_list_                                                                                                                                                                                           
+    del new_list_                                                                                                                                                                                                  
+                                                                                                                                                                                                                   
+    return_list = []                                                                                                                                                                                               
+    for number in new_list:                                                                                                                                                                                        
+        add_num = ""                                                                                                                                                                                               
+        for char in list(number):                                                                                                                                                                                  
+            add_num += ' '+str(9-int(char))                                                                                                                                                                        
+                                                                                                                                                                                                                   
+        add_num = add_num[:-1] + str(1+int(add_num[-1]))                                                                                                                                                           
+                                                                                                                                                                                                                   
+        return_list.append(add_num)                                                                                                                                                                                
+                                                                                                                                                                                                                   
+    # --- Remove numbers greater than 10 ---                                                                                                                                                                       
+    num_index = 0                                                                                                                                                                                                  
+    for cool_thing in return_list:                                                                                                                                                                                 
+        #for i in range(len(cool_thing.strip().split(' '))):                                                                                                                                                       
+        thing_index = 0                                                                                                                                                                                            
+        cool_thing = cool_thing.strip()                                                                                                                                                                            
+        for thing in cool_thing.strip().split(' '):                                                                                                                                                                
+            thing = thing.replace(' ', '')                                                                                                                                                                         
+            if thing:                                                                                                                                                                                              
+                if int(thing.strip()) > 9:                                                                                                                                                                         
+                    return_list[num_index] =' '+cool_thing[:thing_index-2]+ str(int(cool_thing[thing_index-1])+1)  + '0'                                                                                           
+            thing_index += 1                                                                                                                                                                                       
+        num_index += 1                                                                                                                                                                                             
+    for thing in return_list:                                                                                                                                                                                      
+        print("{:,}".format(int(thing.replace(' ', ''))))                                                                                                                                                          
+    print(return_list) 
