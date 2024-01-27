5fcea91120bdc845bd3915b377f40bf94ae022e4
Blue-Robin-Taken <86581171+Blue-Robin-Taken@users.noreply.github.com>
2024-01-27 05:49:01 +0000

    15s complement

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
 import re
 
 complement = 15
+base = 16
 ALPHABET = {'a':10, 'b':11,'c':12,'d':13,'e':14,'f':15}
 
 def hex_switch_space(n):
     """
     Function to change hex alphabet codes to 
-    normal numbers with spaces separating each number.
+    normal numbers in a list.
     This is used for the below processes for calculating
     the complement of the number.
     """
     r_list = []  # return list
     for char in n:
-        if char.lower in ALPHABET.keys():
-            r_list += " " + str(ALPHABET[char])
+        if char.lower() in ALPHABET.keys():
+            r_list.append(str(ALPHABET[char]))
+        elif char.replace(' ', ''):
+            r_list.append(str(char))
+    return r_list
 
+def back_to_hex(n):
+    """
+    Take a string of hex_switch_space but each
+    number in the number is separated by a space instead
+    of being in a list.
+    """
+    r_list = []
+    for char in n.split(' '):
+        char = char.strip()
+        if char.replace(" ", ''):
+            if int(char) in list(ALPHABET.values()):
+                r_list.append(str(list(ALPHABET.keys())[list(ALPHABET.values()).index(int(char))]))
+                #  https://stackoverflow.com/questions/8023306/get-key-by-value-in-dictionary
+            else:
+                r_list.append(char)
+    return r_list
 
 
 with open('input.txt', 'r') as f:
@@ -24,11 +44,11 @@ with open('input.txt', 'r') as f:
         if complement < 15:  # only works for things that don't have letters
             new_list.append(re.sub("[^0-9]", "", line))
         else:
-            line_list = []  # list of things to replace line
-            for char in line:
+            line = line[-11:]
+            new_list.append(hex_switch_space(line))
+    print(new_list[4])
 
-            new_list.append()
-
+    # --- Remove empty strings/lists from list ---
     new_list_ = []
     for line in new_list:
         if line:
@@ -37,6 +57,8 @@ with open('input.txt', 'r') as f:
     del new_list_
 
     return_list = []
+
+    # --- Base calculations ---
     for number in new_list:
         add_num = ""
         begin_count = False
@@ -52,7 +74,19 @@ with open('input.txt', 'r') as f:
                         add_num += " 0"
                 else:
                     add_num += " 0"
-            print(add_num)
+            if complement == 15 or complement == 16:
+                if int(char) != 0 and 15 - int(char) > 0:
+                    begin_count = True
+
+                if begin_count:
+                    if 15 - int(char) > 0:
+                        begin_count = True
+                        add_num += ' '+str((15)-int(char))
+                    else:
+                        add_num += ' ' + '0'
+                        
+        print(add_num)
+
         if complement == 10:
             add_num = add_num[:-1] + str(1+int(add_num[-1]))
         if complement == 8:
@@ -61,7 +95,7 @@ with open('input.txt', 'r') as f:
         return_list.append(add_num)
 
     if complement == 10 or complement == 8:
-        # --- Remove numbers greater than 10 ---
+        # --- Remove numbers greater than complement base ---
         num_index = 0
         for cool_thing in return_list:
             #for i in range(len(cool_thing.strip().split(' '))):
@@ -70,7 +104,7 @@ with open('input.txt', 'r') as f:
             for thing in cool_thing.strip().split(' '):
                 thing = thing.replace(' ', '')
                 if thing:
-                    if int(thing.strip()) > complement-1:
+                    if int(thing.strip()) > base-1:
                         return_list[num_index] =' '+"".join(cool_thing.split(' ')[:thing_index-1])+ str(int(cool_thing.split(' ')[thing_index-1])+1)  + '0'
                 thing_index += 1
             num_index += 1
@@ -86,4 +120,19 @@ with open('input.txt', 'r') as f:
                 a = a[1:]
             print(' '.join(a))
             # https://stackoverflow.com/questions/15254195/python-how-to-add-space-on-each-3-characters
+    if complement == 15 or complement == 16:
+        for thing in return_list:
+            a = "".join(back_to_hex(thing))
+            if len(a)%2 > 0:
+                a = "0" + a
+            new_a = ""
+            i=0
+            for char in a:
+                if i == 2:
+                    i=0
+                    new_a+=" "
+                new_a += char
+                i+=1
+            print(new_a)
+    
     print(return_list)
